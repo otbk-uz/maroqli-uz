94156ab6ca84b1bc9956e8c16de7ffd2a786ec60
Otabek Ravshanov <otabekxoff@gmail.com>
2026-07-12 01:55:03 +0500

    chore: update gamedev lessons insert script with all 6 lessons ordered

--- supabase/migrations/supabase_gamedev_lessons_insert.sql
+++ supabase/migrations/supabase_gamedev_lessons_insert.sql
@@ -1,7 +1,8 @@
 -- Insert new gamedev lessons
 INSERT INTO public.gamedev_lessons (title, author, level, img, video_url) VALUES
-('2-dars: O''yinlardagi qiyinchilik', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://36f9b8b0-3381-4eb6-a9fa-20cc114f39bd'),
-('3-dars: O''yinchini zeriktirmaslik siri - O''yin dizaynida ritm', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://f5ab09e5-24f9-4c9d-aaf3-3ba84ee176a1'),
-('4-dars: O''yinlarda "syujet" qurish', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://5b419d65-e9da-4d5e-9a52-c7c4c615e7a5'),
-('5-dars: Jang sahnalarini "nima" qiziqarli qiladi?', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://42bc9d76-a9f6-4653-b050-158d004dd830'),
-('6-dars: O''yiningizni qanday chiroyli qilish mumkin?', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://d9ed7392-d9a2-4418-828d-96783bc1c863');
+('1-Dars: O''yin dizaynining asosiy tamoyillari', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://f8d26b4f-bdab-4ecd-a249-0dc27dcc0716'),
+('2-Dars: O''yinlardagi qiyinchilik', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://36f9b8b0-3381-4eb6-a9fa-20cc114f39bd'),
+('3-Dars: O''yinchini zeriktirmaslik siri - O''yin dizaynida ritm', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://f5ab09e5-24f9-4c9d-aaf3-3ba84ee176a1'),
+('4-Dars: O''yinlarda "syujet" qurish', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://5b419d65-e9da-4d5e-9a52-c7c4c615e7a5'),
+('5-Dars: Jang sahnalarini "nima" qiziqarli qiladi?', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://42bc9d76-a9f6-4653-b050-158d004dd830'),
+('6-Dars: O''yiningizni qanday chiroyli qilish mumkin?', 'Maroqli.uz', 'O''yin dizayni (boshlang''ich)', 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070', 'bunny://d9ed7392-d9a2-4418-828d-96783bc1c863');
