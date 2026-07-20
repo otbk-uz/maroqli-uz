d9b91e23486d089f590a7c803b3d6eb6f57b6170
otbk-uz <otabekxoff@gmail.com>
2026-07-20 15:27:29 +0500

    feat: add GameDev 0dan o'rganish playlist SQL migration and custom lesson thumbnails

---
+++ supabase/migrations/supabase_gamedev_0dan_organish.sql
@@ -0,0 +1,7 @@
+-- GameDev 0dan o'rganish pleylisti uchun darslarni qo'shish
+INSERT INTO public.gamedev_lessons (title, author, level, img, video_url) VALUES
+('1-Dars: Kirish va gamedev asoslari', 'Uz1games', 'GameDev 0dan o''rganish', '/gamedev_lesson1.png', 'bunny://9ffc7165-53f4-4e5f-baba-d2f64510471b'),
+('2-Dars: Skriptlar va kodlash dunyosi', 'Uz1games', 'GameDev 0dan o''rganish', '/gamedev_lesson2.png', 'bunny://c676c943-945b-48d5-9671-277cd0bdbba4'),
+('3-Dars: 2D va 3D grafika hamda animatsiyalar', 'Uz1games', 'GameDev 0dan o''rganish', '/gamedev_lesson3.png', 'bunny://7c2d0882-6fb3-4ece-b1aa-0c50831ee917'),
+('4-Dars: Birinchi o''yinni yaratish', 'Uz1games', 'GameDev 0dan o''rganish', '/gamedev_lesson4.png', 'bunny://f3ecf61c-9ed6-4ca3-a55a-3ae91ec61204'),
+('5-Dars: O''yinni eksport qilish va nashr etish', 'Uz1games', 'GameDev 0dan o''rganish', '/gamedev_lesson5.png', 'bunny://cf31a726-d2d4-4c3e-8a6c-346018efee8f');
